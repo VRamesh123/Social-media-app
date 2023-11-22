e21e364e100826dca5092a1c928e693369a9bb00
VRamesh123 <105395745+VRamesh123@users.noreply.github.com>
2023-11-22 09:45:48 -0600

    Add files via upload

---
+++ kwitter/let's chat/chat.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc3zNJOHa7txaj--eRl1sIYt32Gf8a0LM",
+  authDomain: "chat-room-1290e.firebaseapp.com",
+  databaseURL: "https://chat-room-1290e-default-rtdb.firebaseio.com",
+  projectId: "chat-room-1290e",
+  storageBucket: "chat-room-1290e.appspot.com",
+  messagingSenderId: "945170865158",
+  appId: "1:945170865158:web:054a04edb9a7424f507787"
+};
+
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
